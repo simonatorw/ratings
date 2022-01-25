4b8aa8b9a0f1b91ae86bc0f482951dc2678f6434
simonatorw <simonatorw@gmail.com>
2022-01-25 18:47:35 -0500

    Added hover

--- rateMe.js
+++ rateMe.js
@@ -5,6 +5,8 @@ template.innerHTML = `
 		display: inline-block;
 		color: #666;
 		cursor: pointer;
+		font-size: 38px;
+		padding: 10px;
 	}
 	
 	.star::after {
@@ -12,7 +14,11 @@ template.innerHTML = `
 	}
 
 	.selected {
-		color: cyan;
+		color: #9ff;
+	}
+
+	.hovered {
+		color: #cff;
 	}
 </style>
 <div>
@@ -36,8 +42,25 @@ export class RateMe extends HTMLElement {
 	connectedCallback() {
 		console.log('Rate Me added to DOM');
 		const star = this.shadowRoot.querySelector('.stars');
+		let max = 0;
 
 		star.addEventListener('click', e => {
+			const el = e.target;
+			const cls = e.target.className;
+			if (cls) {
+				max = parseInt(cls.split(' ')[1]?.split('-')[1], 10);
+				
+				for (let i = 1; i < 6; i++) {
+					if (i <= max) {
+						star.querySelector(`.star:nth-child(${i})`).className = `star star-${i} selected`;
+					} else {
+						star.querySelector(`.star:nth-child(${i})`).className = `star star-${i}`;
+					}
+				}
+			}
+		}, false);
+
+		star.addEventListener('mouseover', e => {
 			const el = e.target;
 			const cls = e.target.className;
 			if (cls) {
@@ -45,6 +68,19 @@ export class RateMe extends HTMLElement {
 				
 				for (let i = 1; i < 6; i++) {
 					if (i <= num) {
+						star.querySelector(`.star:nth-child(${i})`).className = `star star-${i} hovered`;
+					} else {
+						star.querySelector(`.star:nth-child(${i})`).className = `star star-${i}`;
+					}
+				}
+			}
+		}, false);
+
+		star.addEventListener('mouseout', e => {
+			const cls = e.target.className;
+			if (cls) {				
+				for (let i = 1; i < 6; i++) {
+					if (i <= max) {
 						star.querySelector(`.star:nth-child(${i})`).className = `star star-${i} selected`;
 					} else {
 						star.querySelector(`.star:nth-child(${i})`).className = `star star-${i}`;
